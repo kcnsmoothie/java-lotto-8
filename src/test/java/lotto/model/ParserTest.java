27824257015d373db4c3a9dc017ffcbc1e820d5f
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-01 12:52:46 +0900

    test: 문자열 파싱, 정수형 리스트 변환 단위 테스트 추가

--- src/test/java/lotto/model/ParserTest.java
+++ src/test/java/lotto/model/ParserTest.java
@@ -1,7 +1,10 @@
 package lotto.model;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +16,26 @@ public class ParserTest {
 
         assertThatThrownBy(() -> Parser.stringToInt(string))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @Test
+    @DisplayName("구분자(,)단위로 파싱 및 순서대로 저장 검사")
+    void parseWinningNumber_test(){
+        //given
+        String string = "1,2,3,4";
+        //when
+        List<String> parsedWinningNumbers = Parser.parseWinningNumber(string);
+        //then
+        assertThat(parsedWinningNumbers).elements(0, 1, 2, 3).contains("1", "2", "3", "4");
+    }
+
+    @Test
+    @DisplayName("문자열 리스트를 정수형 리스트로 변환해주는 검사")
+    void stringListToIntList_test(){
+        //given
+        List<String> list = Arrays.asList("1","2","3","4");
+        //when
+        List<Integer> winningNumber = Parser.stringListToIntList(list);
+        //then
+        assertThat(winningNumber).elements(0, 1, 2, 3).contains(1,2,3,4);
     }
 }
