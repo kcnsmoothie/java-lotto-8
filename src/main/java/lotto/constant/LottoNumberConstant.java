d371d8ec2a60a2feccc6a2bd802541dd5c960148
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-01 23:42:37 +0900

    feat: 입출력 메시지, 정수 상수화

--- src/main/java/lotto/constant/LottoNumberConstant.java
+++ src/main/java/lotto/constant/LottoNumberConstant.java
@@ -1,7 +1,10 @@
 package lotto.constant;
 
 public enum LottoNumberConstant {
-    LOTTO_PRICE(1000);
+    LOTTO_PRICE(1000),
+    LOTTO_RANGE_MIN(1),
+    LOTTO_RANGE_MAX(45),
+    LOTTO_NUMBER_SIZE(6);
 
     private final int intValue;
 
--- src/main/java/lotto/constant/ViewMessage.java
+++ src/main/java/lotto/constant/ViewMessage.java
@@ -3,16 +3,31 @@ package lotto.constant;
 public enum ViewMessage {
     PURCHASE_AMOUNT_MESSAGE("구입금액을 입력해 주세요."),
     WINNING_NUMBER_MESSAGE("당첨 번호를 입력해 주세요."),
-    BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요.");
+    BONUS_NUMBER_MESSAGE("보너스 번호를 입력해 주세요."),
 
-    private final String inputMessage;
+    PURCHASE_RESULT_MESSAGE("%d개를 구매했습니다."),
+    STATISTICS_HEADER("당첨 통계\n---"),
+
+    FIFTH_PLACE_MESSAGE("3개 일치 (5,000원) - %d개"),
+    FOURTH_PLACE_MESSAGE("4개 일치 (50,000원) - %d개"),
+    THIRD_PLACE_MESSAGE("5개 일치 (1,500,000원) - %d개"),
+    SECOND_PLACE_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIRST_PLACE_MESSAGE("6개 일치 (2,000,000,000원) - %d개"),
+
+    TOTAL_PROFIT_RATE_MESSAGE("총 수익률은 %.1f%%입니다.");
+
+    private final String viewMessage;
 
     ViewMessage(String inputMessage) {
-        this.inputMessage = inputMessage;
+        this.viewMessage = inputMessage;
     }
 
-    public String getInputMessage() {
-        return inputMessage;
+    public String getViewMessage() {
+        return viewMessage;
+    }
+
+    public String getViewMessage(Integer value) {
+        return String.format(viewMessage, value);
     }
 }
 
