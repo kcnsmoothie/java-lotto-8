9e7a151b5ca0267e4ccca4c1d1ef7413994e3a40
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-01 23:39:49 +0900

    feat: 로또 발행 기능 구현

--- src/main/java/lotto/model/LottoService.java
+++ src/main/java/lotto/model/LottoService.java
@@ -1,6 +1,17 @@
 package lotto.model;
 
+import static lotto.constant.LottoNumberConstant.LOTTO_NUMBER_SIZE;
 import static lotto.constant.LottoNumberConstant.LOTTO_PRICE;
+import static lotto.constant.LottoNumberConstant.LOTTO_RANGE_MAX;
+import static lotto.constant.LottoNumberConstant.LOTTO_RANGE_MIN;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import lotto.Lotto;
 
 public class LottoService {
     public LottoService() {
@@ -8,5 +19,29 @@ public class LottoService {
 
     public int calculatePurchasableLottoCount(int purchaseAmount) {
         return purchaseAmount / LOTTO_PRICE.getValue();
+    }
+
+    //1~45 사이에서 중복되지 않는 숫자 6개를 뽑는 기능
+    public List<Integer> makeRandomLottoNumbers() {
+        List<Integer> randomLottoNumber = Randoms.pickUniqueNumbersInRange(LOTTO_RANGE_MIN.getValue(),
+                LOTTO_RANGE_MAX.getValue(),
+                LOTTO_NUMBER_SIZE.getValue());
+        return randomLottoNumber;
+    }
+
+    //오름차순으로 로또를 정렬하는 기능
+    public void sortAscending(List<Integer> randomLottoNumber) {
+        Collections.sort(randomLottoNumber);
+    }
+
+    //발행 수량만큼 로또 발행
+    public List<Lotto> lottoMaker(int lottoCount){
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            List<Integer> numbers = makeRandomLottoNumbers();
+            sortAscending(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
     }
 }
