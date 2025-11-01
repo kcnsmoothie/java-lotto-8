c5b0f6aaaf023bcf4a14023291b4e8b51220fae2
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-01 13:39:11 +0900

    test: 구입 금액이 1000원 단위 검사 단위 테스트 추가

--- src/test/java/lotto/model/ValidatorTest.java
+++ src/test/java/lotto/model/ValidatorTest.java
@@ -1,7 +1,10 @@
 package lotto.model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,6 +16,17 @@ public class ValidatorTest {
     @DisplayName("입력값이 null 또는 공백인지 검사")
     void validateNotBlank_test(String inputValue) {
         assertThatThrownBy(() -> Validator.validateNotBlank(inputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("구입 금액이 1000원 단위로 나누어 떨어지는지 검사 - 실패")
+    void validateNotMultipleOfThousand_test() {
+        //given
+        int inputValue = 1249;
+
+        //when & then
+        assertThatThrownBy(() -> Validator.validateNotMultipleOfThousand(inputValue))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
