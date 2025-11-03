37050e5b63719376e9e036dd175fcd29a9458c41
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-03 11:24:28 +0900

    refactor: 패키지 구조 변경으로 코드 계층 정리

---
+++ src/main/java/lotto/util/Parser.java
@@ -0,0 +1,35 @@
+package lotto.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static lotto.constant.ErrorMessage.*;
+
+public class Parser {
+    public static int stringToInt(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_ERROR.getErrorMessage());
+        }
+    }
+
+    public static List<String> parseWinningNumber(String inputWinningNumber) {
+        List<String> parsedWinningNumber = new ArrayList<>();
+        Arrays.stream(inputWinningNumber.split(","))
+                .map(String::trim)
+                .forEach(parsedWinningNumber::add);
+        return parsedWinningNumber;
+    }
+
+    public static List<Integer> stringListToIntList(List<String> parsedWinningNumber) {
+        try {
+            return parsedWinningNumber.stream()
+                    .map(Integer::parseInt)
+                    .toList();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_NUMBER_FORMAT_ERROR.getErrorMessage());
+        }
+    }
+}
