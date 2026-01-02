c9c01a8df019e0c92cfa0e3f28a5bc799597c1f4
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-01-02 17:50:45 +0900

    feat: 로또 수익률 출력 기능 구현

--- src/main/java/lotto/constant/ViewMessage.java
+++ src/main/java/lotto/constant/ViewMessage.java
@@ -1,7 +1,16 @@
 package lotto.constant;
 
 public enum ViewMessage {
-    PURCHASE_AMOUNT("%d개를 구매했습니다.");
+    PURCHASE_AMOUNT("%d개를 구매했습니다."),
+
+    HEADER("당첨 통계\n" + "---"),
+    FIRST_MESSAGE("3개 일치 (5,000원) - %d개"),
+    SECOND_MESSAGE("4개 일치 (50,000원) - %d개"),
+    THIRD_MESSAGE("5개 일치 (1,500,000원) - %d개"),
+    FOURTH_MESSAGE("5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    FIFTH_MESSAGE("6개 일치 (2,000,000,000원) - %d개"),
+
+    PROFIT_RATE_MESSAGE("총 수익률은 %.1f%%입니다.");
 
     private final String viewMessage;
 
@@ -14,6 +23,10 @@ public enum ViewMessage {
     }
 
     public String getViewMessage(int value) {
+        return String.format(viewMessage, value);
+    }
+
+    public String getViewMessage(double value) {
         return String.format(viewMessage, value);
     }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,17 +1,33 @@
 package lotto.view;
 
+import static lotto.constant.ViewMessage.FIFTH_MESSAGE;
+import static lotto.constant.ViewMessage.FIRST_MESSAGE;
+import static lotto.constant.ViewMessage.FOURTH_MESSAGE;
+import static lotto.constant.ViewMessage.HEADER;
+import static lotto.constant.ViewMessage.PROFIT_RATE_MESSAGE;
 import static lotto.constant.ViewMessage.PURCHASE_AMOUNT;
+import static lotto.constant.ViewMessage.SECOND_MESSAGE;
+import static lotto.constant.ViewMessage.THIRD_MESSAGE;
+
+import java.util.Map;
+import lotto.constant.Rank;
 
 public class OutputView {
     public void printLottoAmount(int lottoAmount) {
         System.out.println(PURCHASE_AMOUNT.getViewMessage(lottoAmount));
     }
 
-    public void printLottoHit() {
+    public void printLottoHit(Map<Rank, Integer> rankResult) {
+        System.out.println(HEADER.getViewMessage());
+        System.out.println(FIRST_MESSAGE.getViewMessage());
+        System.out.println(SECOND_MESSAGE.getViewMessage());
+        System.out.println(THIRD_MESSAGE.getViewMessage());
+        System.out.println(FOURTH_MESSAGE.getViewMessage());
+        System.out.println(FIFTH_MESSAGE.getViewMessage());
     }
 
-    public void printProfitRate() {
-
+    public void printProfitRate(double profitRate) {
+        System.out.println(PROFIT_RATE_MESSAGE.getViewMessage(profitRate));
     }
 
     public void printErrorMessage(IllegalArgumentException e) {
